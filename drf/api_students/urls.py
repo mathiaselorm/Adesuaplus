3ce33b4197c6723df33e9d374d3177bdb5c282e2
mathiaselorm <mathiaselorm@gmail.com>
2024-02-11 23:13:42 +0000

    endpoint naming

--- drf/api_students/urls.py
+++ drf/api_students/urls.py
@@ -1,9 +1,9 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('list_students/', views.list_students, name='students'),
-    path('create_student/', views.create_student, name='create_student'),
-    path('retrieve_student/<str:pk>/', views.retrieve_student, name='retrieve_student'),
-    path('update_student/<str:pk>/', views.update_student, name='students_update'),
-    path('delete_student/<str:pk>/', views.delete_student, name='students_delete'),
+    path('students-list/', views.list_students, name='students-list'),
+    path('create-student/', views.create_student, name='create-student'),
+    path('retrieve/student/<str:pk>/', views.retrieve_student, name='retrieve-student'),
+    path('update/student/<str:pk>/', views.update_student, name='update-student'),
+    path('delete/student/<str:pk>/', views.delete_student, name='delete-student'),
 ]
