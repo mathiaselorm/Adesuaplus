7ebb858724869dbeecccf93b34e62ada433c78ba
mathiaselorm <mathiaselorm@gmail.com>
2024-03-09 08:07:51 +0000

    student api
    Student api that allow CRUD operation

--- drf/administrators/serializers.py
+++
@@ -1,9 +0,0 @@
-from . models import Administrators
-from rest_framework import serializers
-
-
-class AdministratorsSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Administrators
-        fields = '__all__'
--- drf/teachers/serializers.py
+++
@@ -1,9 +0,0 @@
-from . models import Teachers
-from rest_framework import serializers
-
-
-class TeachersSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Teachers
-        fields = '__all__'
